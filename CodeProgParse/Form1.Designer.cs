76d2eabfb4fd80bc98861bdeb243d20d300b5bf7
vladimir26061984 <vladimir.chemir@yandex.ru>
2020-03-14 19:20:16 +0300

    Первый коммит

---
+++ CodeProgParse/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace CodeProgParse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Корень";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(63, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1008, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "D:\\Прогноз\\Prognoz.Net.git\\trunk\\Prognoz.Net\\bin\\Release";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1077, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Задать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "cs",
+            "csproj",
+            "xml",
+            "resx",
+            "config",
+            "sln"});
+            this.listBox1.Location = new System.Drawing.Point(15, 61);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(121, 134);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Перечень целевых расширений файлов";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(142, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(142, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(232, 62);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(928, 481);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(15, 516);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(202, 27);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Пуск";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(142, 337);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Удалить";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(142, 226);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Добавить";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "\\obj\\"});
+            this.listBox2.Location = new System.Drawing.Point(15, 226);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(121, 134);
+            this.listBox2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(211, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Перечень исключений имен директорий";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(17, 384);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(182, 17);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Писать в файл textProgramm.txt";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1172, 555);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Получение списка файлов с количеством строк";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ CodeProgParse/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.IO;
+
+namespace CodeProgParse
+{
+    public partial class Form1 : Form
+    {
+        List<string> ext_list = new List<string>();
+        List<string> dir_list = new List<string>();
+        int index = 0;
+        bool flag_stop = false;
+        string result_path = "textProgramm.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(result_path, "");
+            index = 0;
+            flag_stop = false;
+            ext_list.Clear();
+            ext_list.AddRange(listBox1.Items.OfType<string>());
+
+            dir_list.Clear();
+            dir_list.AddRange(listBox2.Items.OfType<string>());
+
+
+            string path = textBox1.Text.Trim();
+            if (File.Exists(path))
+            {
+                ProcessFile(path);
+            }
+            else if (Directory.Exists(path))
+            {
+                ProcessDirectory(path);
+            }
+            else
+            {
+                MessageBox.Show("Задайте другой путь", "Ошибка выбранного пути", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void ProcessDirectory(string targetDirectory)
+        {
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+            {
+                if (!flag_stop)
+                    ProcessFile(fileName);
+            }
+
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+            {
+                bool valid = true;
+                foreach (string param in dir_list)
+                {
+                    if (subdirectory.Contains(param))
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+
+                if (!valid)
+                    continue;
+
+                if (!flag_stop)
+                    ProcessDirectory(subdirectory);
+            }
+        }
+
+        public void ProcessFile(string path)
+        {
+
+            var t = Path.GetExtension(path).Replace(".", "");
+            if (ext_list.Contains(t))
+            {
+                if (index > 0)
+                    textBox2.Text += Environment.NewLine + (index + 1).ToString() + "\t" + getPath(path) + "\t" + getNumbersLines(path);
+                else
+                    textBox2.Text = (index + 1).ToString() + "\t" + getPath(path) + "\t" + getNumbersLines(path);
+                index++;
+                textBox2.Update();
+            }
+            else if (t.Equals("dll") || t.Equals("exe"))
+            {
+                if (index > 0)
+                    textBox2.Text += Environment.NewLine + (index + 1).ToString() + "\t" + getPath(path) + "\t" + getSize(path);
+                else
+                    textBox2.Text = (index + 1).ToString() + "\t" + getPath(path) + "\t" + getSize(path);
+                index++;
+                textBox2.Update();
+            }
+                
+            //if (index == 10)
+            //    flag_stop = true;
+        }
+
+        public string getPath(string path)
+        {
+            return path.Replace(textBox1.Text,"");
+        }
+
+        public string getSize(string path)
+        {
+            long length = new System.IO.FileInfo(path).Length;
+            return length.ToString();
+        }
+
+        public string getNumbersLines(string path)
+        {
+            string filename = Path.GetFileName(path);
+            var lines = File.ReadAllLines(path).ToList();
+            if (checkBox1.Checked)
+            {
+                File.AppendAllText(result_path, Environment.NewLine + "[Ё]" + filename + Environment.NewLine + Environment.NewLine);
+                foreach (string SSS in lines)
+                {
+                    File.AppendAllText(result_path, SSS + Environment.NewLine);
+                }
+            }
+            //File.AppendAllText(result_path, lines.ToString());
+            return lines.Count().ToString();
+        }
+    }
+}
